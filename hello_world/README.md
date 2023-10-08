c49e688b2fea330cde00d5a0ed6619bf290a9658
Jaeha Lee <wogkdkrm112@gmail.com>
2023-10-09 01:44:35 +0900

    init: axum tokio 프로젝트 세팅

---
+++ hello_world/README.md
@@ -0,0 +1,8 @@
+## 프로젝트 세팅
+
+```bash
+cargo init
+cargo add axum
+cargo add tokio
+cargo add tokio -F macros -F rt-multi-thread
+```
---
+++ hello_world/src/main.rs
@@ -0,0 +1,16 @@
+use axum::{
+    routing::get,
+    Router,
+};
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    // run it with hyper on localhost:3000
+    axum::Server::bind(&"http://localhost:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
