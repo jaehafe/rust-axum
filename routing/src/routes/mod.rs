36dd010d1159063ff0bbb7348075f439506e33c4
Jaeha Lee <wogkdkrm112@gmail.com>
2023-10-09 20:26:58 +0900

    feat: http-tower(feature cors) cors middleware

--- routing/src/routes/mod.rs
+++ routing/src/routes/mod.rs
@@ -9,7 +9,7 @@ mod mirror_custom_header;
 use axum::{
   Router,
   routing::{get, post, patch},
-  body::Body
+  body::Body, http::Method
 };
 use hello_world::hello_world;
 use mirror_body_string::mirror_body_string;
@@ -18,8 +18,14 @@ use path_variables::{path_variables, hard_coded_path};
 use query_params::query_params;
 use mirror_user_agent::mirror_user_agent;
 use mirror_custom_header::mirror_custom_header;
+use tower_http::cors::{Any, CorsLayer};
+
 
 pub fn create_routes() -> Router {
+  let cors = CorsLayer::new()
+    .allow_methods([Method::GET, Method::POST])
+    .allow_origin(Any);
+
   Router::new()
     .route("/", get(hello_world))
     .route("/mirror_body_string", post(mirror_body_string))
@@ -29,4 +35,5 @@ pub fn create_routes() -> Router {
     .route("/query_params", get(query_params))
     .route("/mirror_user_agent", get(mirror_user_agent))
     .route("/mirror_custom_header", get(mirror_custom_header))
+    .layer(cors)
 }
