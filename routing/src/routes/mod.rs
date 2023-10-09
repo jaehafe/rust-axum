abafd97f1241d255ba7b72f6dade40b8f31fde42
Jaeha Lee <wogkdkrm112@gmail.com>
2023-10-09 18:33:32 +0900

    feat: user agent - extracting headers

--- routing/src/routes/mod.rs
+++ routing/src/routes/mod.rs
@@ -3,6 +3,7 @@ mod mirror_body_string;
 mod mirror_body_json;
 mod path_variables;
 mod query_params;
+mod mirror_user_agent;
 
 use axum::{
   Router,
@@ -14,6 +15,7 @@ use mirror_body_string::mirror_body_string;
 use mirror_body_json::mirror_body_json;
 use path_variables::{path_variables, hard_coded_path};
 use query_params::query_params;
+use mirror_user_agent::mirror_user_agent;
 
 pub fn create_routes() -> Router {
   Router::new()
@@ -23,4 +25,5 @@ pub fn create_routes() -> Router {
     .route("/path_variables/15", get(hard_coded_path))
     .route("/path_variables/:id", get(path_variables))
     .route("/query_params", get(query_params))
+    .route("/mirror_user_agent", get(mirror_user_agent))
 }
