63bf490a3e8e0beeb6395b95482fd0f64e0aba43
jaeha <wogkdkrm112@gmail.com>
2023-10-12 10:40:00 +0900

    feat: create task

--- seaorm/src/routes/mod.rs
+++ seaorm/src/routes/mod.rs
@@ -1,15 +1,19 @@
 #![allow(unused)]
 
 mod custom_json_extractor;
+mod validate_with_serde;
+mod create_task;
 
 use axum::{
     extract::FromRef,
     middleware,
     routing::{delete, get, patch, post, put},
-    Router,
+    Router, Extension,
 };
 use custom_json_extractor::custom_json_extractor;
 use sea_orm::DatabaseConnection;
+use validate_with_serde::validate_with_serde;
+use create_task::create_task;
 
 #[derive(Clone, FromRef)]
 pub struct AppState {
@@ -17,9 +21,12 @@ pub struct AppState {
 }
 
 pub async fn create_routes(database: DatabaseConnection) -> Router {
-    let app_state = AppState { database };
+    // let app_state = AppState { database };
     Router::new()
         // .route_layer(middleware::from_fn_with_state(app_state.clone(), guard))
+        .route("/validate_data", post(validate_with_serde))
         .route("/custom_json_extractor", post(custom_json_extractor))
-        .with_state(app_state)
+        .route("/tasks", get(create_task))
+        .layer(Extension(database))
+        // .with_state(app_state)
 }
